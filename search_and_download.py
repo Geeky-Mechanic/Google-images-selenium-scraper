06ceb0abd02073fc4b14e315564434fb8c0446ca
Jeremy Champagne <jeremychampagne@hotmail.com>
2023-01-09 20:02:53 -0500

    fix issues

--- search_and_download.py
+++ search_and_download.py
@@ -11,12 +11,18 @@ def search_and_download(search_term: str, driver_path: str, target_path='./image
     # Create the webdriver service
     service = Service(driver_path)
 
+    # add some options for the chrome browser
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
     target_folder = os.path.join(target_path, '_'.join(search_term.lower().split(' ')))
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
 
-    with webdriver.Chrome(service=service) as wd:
+    with webdriver.Chrome(service=service, chrome_options=chrome_options) as wd:
         res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0.5)
 
     for elem in res:
